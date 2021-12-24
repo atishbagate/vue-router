37701e9de78c3f768bbc30802d189329cbe71ea2
atish <atishbagate.aplswd@gmail.com>
2021-12-24 11:59:01 +0530

    Creating a Go-Back button

---
+++ src/components/GoBack.vue
@@ -0,0 +1,25 @@
+<template>
+  <span class="go-back">
+    <button @click="goBack">Go Back</button>
+  </span>
+</template>
+<script>
+export default {
+  methods: {
+    goBack() {
+      // thi s syntax will tell you how many steps to go back to visited page.
+      // reference will get from history stack in router.
+      return this.$router.go(-1);
+    },
+  },
+};
+</script>
+<style scoped>
+.go-back {
+  display: flex;
+  cursor: pointer;
+}
+button {
+  border: 0;
+}
+</style>
--- src/views/DestinationDetails.vue
+++ src/views/DestinationDetails.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <GoBack />
     <section class="destination">
       <h1>{{ destination.name }}</h1>
       <div class="destination-details">
@@ -41,9 +42,10 @@
 </template>
 <script>
 import store from "@/store.js";
+import GoBack from "@/components/GoBack";
 export default {
-  data() {
-    return {};
+  components: {
+    GoBack,
   },
   props: {
     slug: {
