b8f30ce9137f326aeb9795985de1b96d57f67abf
atish <atishbagate.aplswd@gmail.com>
2021-12-24 12:47:52 +0530

    fade in out transition.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <TheNavigation />
-    <transition name="moveUp">
+    <transition name="fade">
       <router-view :key="$route.path" />
     </transition>
     <!-- <Home /> -->
@@ -26,29 +26,12 @@ export default {
 }
 /* //this is the class name of slide for transition (custome class -> slide)
 there are 6 total classes given by vue to define transition  */
-.moveUp-enter-active {
-  animation: fadeIn 1s ease-in;
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
 }
-@keyframes fadeIn {
-  0% {
-    opacity: 0;
-  }
-  50% {
-    opacity: 0.5;
-  }
-  100% {
-    opacity: 1;
-  }
-}
-.moveUp-leave-active {
-  animation: moveUp 1s ease-in;
-}
-@keyframes moveUp {
-  0% {
-    transform: translateY(0);
-  }
-  100% {
-    transform: translateY(-400px);
-  }
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
